561a7714c0f0cc665ca93305471e9db08e901dc3
GonzaBianchi <gonzalobianchini70@gmail.com>
2024-05-17 17:37:49 -0300

    Integrador finalizado

---
+++ main.js
@@ -0,0 +1,119 @@
+// Codigo Contact
+function validateForm() {
+    let nombre = document.getElementById('nombre').value;
+    let email = document.getElementById('email').value;
+    let telefono = document.getElementById('telefono').value;
+    let consulta = document.getElementById('mensaje').value;
+    let motivo = document.getElementById('sede').value;
+    let conocimiento = document.querySelector('input[name="conocimiento"]:checked');
+  
+    if (!nombre || !email || !telefono || !mensaje || !sede || !conocimiento) {
+      alert('Todos los campos son obligatorios');
+      return false;
+    }
+    return true;
+}
+
+// Codigo Nav y footer
+document.addEventListener('DOMContentLoaded', function() {
+    const checkbox = document.getElementById('check');
+    const openMenu = document.querySelector('.open-menu');
+    const closeMenu = document.querySelector('.close-menu');
+    const menu = document.querySelector('.menu');
+
+    // Event listener para abrir el menú
+    openMenu.addEventListener('click', function() {
+        openMenu.style.display = 'none';
+        closeMenu.style.display = 'block'; 
+        menu.style.display = 'flex'; 
+    });
+
+    // Event listener para cerrar el menú
+    closeMenu.addEventListener('click', function() {
+        openMenu.style.display = 'block'; 
+        closeMenu.style.display = 'none'; 
+        menu.style.display = 'none'; 
+    });
+});
+
+
+
+//comportamiento general
+
+const sections = document.querySelectorAll('.seccion');
+const navLinks = document.querySelectorAll('.navItem');
+let currentSection = null; // Variable para la sección actual
+
+function activateLink(section) {
+    currentSection = section; // Actualiza la variable con la sección actual
+    navLinks.forEach(link => {
+        if (link !== section.querySelector('a')) { // Compara el elemento sección con el elemento a
+            link.classList.remove('active');
+        }
+    });
+  
+    const matchingLink = document.querySelector(`[href="#${section.id}"]`);
+    if (matchingLink) {
+        matchingLink.classList.add('active');
+    }
+  
+    // Verifica si la sección actual es la sección de inicio
+    if (section.id === 'home') {
+        const homeLink = document.querySelector('.navItem[href="#home"]');
+        if (homeLink) {
+            homeLink.classList.add('active');
+        }
+    }
+}
+
+sections.forEach(section => {
+    new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            activateLink(section);
+        }
+        });
+    }, {
+        root: null,
+        threshold: 0.5
+    }).observe(section);
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+        const targetSection = document.getElementById(link.getAttribute('href').slice(1));
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+
+        navLinks.forEach(otherLink => {
+            if (otherLink !== link) { // Compara el elemento a con el elemento a clickeado
+                otherLink.classList.remove('active');
+            }
+        });
+
+        link.classList.add('active');
+    });
+
+    link.addEventListener('mouseover', () => {
+        navLinks.forEach(otherLink => {
+            otherLink.classList.remove('active');
+        });
+
+        link.classList.add('active');
+    });
+
+    link.addEventListener('mouseleave', () => {
+        if (currentSection) {
+            const linkActive = document.querySelector(`[href="#${currentSection.id}"]`);
+            console.log(currentSection.id);
+            if (linkActive) {
+                linkActive.classList.add('active');
+            }
+        }
+        link.classList.remove('active');
+    });
+});
+
+
+
